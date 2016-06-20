72cb5c37a346b14a007342ea190f4d09357ce333
meonbitbucket <dpaudel@automationofthings.com>
2016-06-20 13:54:14 -0400

    added new types

---
+++ camera_rt/ui_client_example/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Input;
+using Prism.Commands;
+using PtzCamera;
+using XNSWindowctrlAxLib;
+
+//using LCM = LCM.LCM;
+
+namespace ui_client_example
+{
+    public class MainWindowViewModel
+    {
+        public ICommand DiscoverCommand { get; set; }
+
+        public MainWindowViewModel()
+        {
+            DiscoverCommand = new DelegateCommand(OnDiscoverCommand);
+        }
+
+        private void OnDiscoverCommand()
+        {
+            TestAssembly();
+            var lcm = LCM.LCM.LCM.Singleton;
+
+            var discoverRequest = new DiscoveryRequest_t();
+            lcm.Publish("PTZCAMERA", discoverRequest);
+        }
+
+        private void TestAssembly()
+        {
+            //var types =  typeof(AxXnsSdkDevice).Assembly.GetExportedTypes();
+            //var attributes = typeof(AxXnsSdkDevice).Assembly.GetCustomAttributes(typeof(ComVisibleAttribute), false);
+            var types =  typeof(XNSWindowctrlAx).Assembly.GetExportedTypes();
+            var attributes = typeof(AxXNSSDKWINDOWLib.AxXnsSdkWindow).Assembly.GetCustomAttributes(typeof(ComVisibleAttribute), false);
+        }
+    }
+}
