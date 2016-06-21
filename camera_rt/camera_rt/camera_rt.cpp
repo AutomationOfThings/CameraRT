a4e1c051b484e17c553fec89927350f2799c3f09
meongithub <meonelectronicmail@gmail.com>
2016-06-21 01:37:37 -0400

    added casablanca changes

--- camera_rt/camera_rt/camera_rt.cpp
+++ camera_rt/camera_rt/camera_rt.cpp
@@ -2,15 +2,24 @@
 //
 
 #include "stdafx.h"
-//#include "..\lcm\lcm.h"
 #include "lcm\lcm-cpp.hpp"
 #include "Types\DiscoveryRequest_t.hpp"
 #include "Types\StreamUriRequest_t.hpp"
 #include "Types\StreamUriResponse_t.hpp"
+#include <cpprest\http_client.h>
+#include <cpprest\filestream.h>
+#include <cpprest/uri.h> 
 #include <iostream>
+#include <string>
 
 #include "udp_socket_provider.h"
 #include "SunApiRequestResponseTypes\BroadcastTypes.h"
+
+using namespace utility;                    // Common utilities like string conversions
+using namespace web;                        // Common features like URIs.
+using namespace web::http;                  // Common HTTP functionality
+using namespace web::http::client;          // HTTP client features
+using namespace concurrency::streams;       // Asynchronous streams
 
 class LcmHandler
 {
@@ -47,10 +56,21 @@ class LcmHandler
 		{
 			std::cout << "Received message on channel: " << channel << std::endl;
 			std::cout << "Received stream request for camera: " << req->cameraName << std::endl;
+			char get_req[400];
+			memcpy(get_req, _main_get_req, 51);
+			memcpy(get_req + 51, _stream_get_req, 77);
+			char buff[200];
+			sprintf_s(get_req, "192.168.0.148", "video", 
+				"stream", "view", "0", "MJPEG", "600x450", "15", "10");
+			
+			//http_client client(U(""));
+
 		}
 
 	 const  char* _discovery_req_channel = "DISCOVERYREQ";
 	 const char* _stream_req_channel = "STREAMURIREQ";
+	 const char* _main_get_req = "http://%s%/stw-cgi/%s%.cgi?msubmenu=%s%&action=%s%";
+	 const char* _stream_get_req = "&Profile=%s%&CodecType=%s%&Resolution=%s%&FrameRate=%s%&CompressionLevel=%s%";
 };
 
 int _tmain(int argc, _TCHAR* argv[])
